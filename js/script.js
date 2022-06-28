4b83a614178a1c93e23193cda8c3543b8308f155
pvl-ao <pvl-ao@pm.me>
2022-06-28 22:08:29 +0300

    Add script

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-'use strict'
+'use strict';
+
+const insert = document.querySelector('#insert');
+
+window.addEventListener('keydown', (event) => {
+  insert.innerHTML = `<div class="key">
+  ${event.key === ' ' ? 'space' : event.key}
+  <small>event.key</small>
+</div>
+<div class="key">
+ ${event.keyCode}
+  <small>event.keyCode</small>
+</div>
+<div class="key">
+${event.code}
+<small>event.code</small>
+</div>`;
+});
